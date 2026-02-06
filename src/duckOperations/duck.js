06fc5d47bd3464a17cf83be6af38ca828b5e1bf7
Dr. Dean von Schoultz <deanvons@gmail.com>
2026-02-06 09:35:17 +0100

    FEAT: deployment

---
+++ src/duckOperations/duck.js
@@ -0,0 +1,7 @@
+export function doSomething() {
+  doSomethingElse()
+}
+
+export function doSomethingElse() {
+  console.log('working')
+}
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { doSomething } from './duckOperations/duck.js'
+
+for (let i = 0; i < 3; i++) {
+  console.log('')
+  console.log('')
+}
+
+console.log('behold another change')
+
+doSomething();
